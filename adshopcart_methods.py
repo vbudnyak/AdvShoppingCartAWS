014e9474d734604756c32d2121617261b3a8fff7
vbudnyak <99363082+vbudnyak@users.noreply.github.com>
2022-03-03 22:29:45 -0800

    Update adshopcart_methods.py

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -4,7 +4,7 @@ import adshopcart_locators as locators
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 
